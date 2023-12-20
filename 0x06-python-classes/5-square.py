1177acbcb2a879dea422d5c92aaaf4235214f98f
sherazme <sheraz.m.e@gmail.com>
2023-12-19 18:57:38 -0800

    python-classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -18,3 +18,35 @@ class Square:
             raise TypeError("size must be an integer")
 
         self.__size = size
+
+    # Getter method
+    @property
+    def size(self):
+        """ Retrive the Size of the Square """
+        return (self.__size)
+
+    # Setter method
+    @size.setter
+    def size(self, size):
+        """ Set the size of a Square """
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+        self.__size = size
+
+    def area(self):
+        """ Return The area of the square """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """ Print the Squar using the charagter # """
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print("#", end="")
+                print()
