e9c58439765f890ce2f770fc609b3079564e215e
sherazme <sheraz.m.e@gmail.com>
2023-12-19 18:59:02 -0800

    python-classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -71,11 +71,11 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        for i in range(self.__position[1]):
+        for x in range(self.__position[1]):
             print("")
-        for j in range(self.__size):
-            for k in range(self.__position[0]):
+        for y in range(self.__size):
+            for z in range(self.__position[0]):
                 print(" ", end="")
-            for l in range(self.__size):
+            for a in range(self.__size):
                 print("#", end="")
-            print()
+       
