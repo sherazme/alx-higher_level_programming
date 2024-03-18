b4d249884784713c56da7da3ff700018789b656b
sherazme <sheraz.m.e@gmail.com>
2024-03-17 19:07:54 -0700

    mysql

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,8 +11,9 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT c.id, c.name, s.name FROM cities c INNER JOIN states s
-            ON c.state_id = s.id ORDER BY c.id")
+    qu = """SELECT c.id, c.name, s.name FROM states s, cities c
+          WHERE c.state_id = s.id ORDER BY c.id ASC"""
+    cur.execute(qu)
 
     rows = cur.fetchall()
     for i in rows:
