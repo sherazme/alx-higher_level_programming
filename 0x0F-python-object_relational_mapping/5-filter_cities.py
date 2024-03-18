2e7e506118dbb749d30e6a25271d9b77228b2e7f
sherazme <sheraz.m.e@gmail.com>
2024-03-17 19:20:51 -0700

    mysql

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+# The code should not be executed when imported
+if __name__ == '__main__':
+
+    # make connection with database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    qu = """SELECT c.name FROM states s INNER JOIN cities c
+    ON c.state_id = s.id WHERE  states.name = %s
+    ORDER BY c.id ASC"""
+    cur.execute(qu, [argv[4]])
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Close process
+    cur.close()
+    db.close()
