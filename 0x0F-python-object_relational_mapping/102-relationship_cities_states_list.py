ea47754341fc054e035a911890dceb676c82d1a4
sherazme <sheraz.m.e@gmail.com>
2024-03-20 20:27:30 -0700

    mapping

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
+'''Prints City objects and State '''
+import sys
+from sqlalchemy import create_engine, and_, text, tuple_
+from sqlalchemy.orm import sessionmaker, relationship
+
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy import create_engine
-import sys
-"""
-performs MySQL query through MySQLAlchemy
-"""
 
 
-if __name__ == "__main__":
-    db_uri = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                                                            sys.argv[1],
-                                                            sys.argv[2],
-                                                            sys.argv[3])
-
-    engine = create_engine(db_uri, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for a_city in session.query(City).order_by(City.id):
-        print("{}: {} -> {}".format(a_city.id, a_city.name, a_city.state.name))
-
-    session.close()
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        DATABASE_URL = 'mysql://{}:{}@localhost:3306/{}'.format(
+            user, pword, db_name
+        )
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        qu = session.query(City).join(State).order_by(City.id.asc())
+        for city in qu.all():
+            print('{}: {} -> {}'.format(city.id, city.name, city.state.name))
