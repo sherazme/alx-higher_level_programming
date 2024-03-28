64e342a8172cce0301d319b1d62e92b13e388bee
sherazme <sheraz.m.e@gmail.com>
2024-03-27 17:09:27 -0700

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,17 +11,17 @@ def find_peak(list_of_integers):
     """
     int_list = list_of_integers
     # if no list of int return None
-    if int_list == []:
+    if ilist == []:
         return None
-    l= len(int_list)
+    length = len(ilist)
 
-    start, end = 0, l- 1
+    start, end = 0, length - 1
     while start < end:
         mid = start + (end - start) // 2
-        if list_[mid] > int_list[mid - 1] and int_list[mid] > int_list[mid + 1]:
-            return int_list[mid]
-        if int_list[mid - 1] > int_list[mid + 1]:
+        if ilist[mid] > ilist[mid - 1] and ilist[mid] > ilist[mid + 1]:
+            return ilist[mid]
+        if ilist[mid - 1] > ilist[mid + 1]:
             end = mid
         else:
             start = mid + 1
-    return int_list[start]
+    return ilist[start]
