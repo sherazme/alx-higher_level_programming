a800acc3513ee8ad86609f2670519b41ee13a572
sherazme <sheraz.m.e@gmail.com>
2024-03-27 17:17:44 -0700

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,19 +9,27 @@ def find_peak(list_of_integers):
 
     Returns: peak
     """
-    int_list = list_of_integers
-    # if no list of int return None
-    if ilist == []:
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
-    length = len(ilist)
 
-    start, end = 0, length - 1
-    while start < end:
-        mid = start + (end - start) // 2
-        if ilist[mid] > ilist[mid - 1] and ilist[mid] > ilist[mid + 1]:
-            return ilist[mid]
-        if ilist[mid - 1] > ilist[mid + 1]:
-            end = mid
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    mid = int(len(list_of_integers) / 2)
+
+    if mid != len(list_of_integers) - 1:
+        if list_of_integers[mid - 1] < list_of_integers[mid] and\
+           list_of_integers[mid + 1] < list_of_integers[mid]:
+            return list_of_integers[mid]
+    else:
+        if list_of_integers[mid - 1] < list_of_integers[mid]:
+            return list_of_integers[mid]
         else:
-            start = mid + 1
-    return ilist[start]
+            return list_of_integers[mid - 1]
+
+    if list_of_integers[mid - 1] > list_of_integers[mid]:
+        ilist = list_of_integers[0:mid]
+    else:
+        ilist = list_of_integers[mid + 1:]
+
+    return find_peak(ilist)
