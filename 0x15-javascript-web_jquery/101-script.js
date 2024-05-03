47682db4a3ee08d1f89b9f26149b7c0f9b48e68b
Shiraz Mohammed Elhassan Osman Elmahi <73830525+sherazme@users.noreply.github.com>
2024-05-03 06:18:03 +0400

    Create 101-script.js

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+//script that adds, removes and clears LI elements from a list
+$('document').ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last').remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
